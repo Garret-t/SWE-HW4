70817a995939370230564aa9446f555563548feb
Garrett Scott <Garret-t@users.noreply.github.com>
2021-04-27 08:51:49 -0700

    Added type test

--- Q1/test_volume.py
+++ Q1/test_volume.py
@@ -2,10 +2,14 @@ import unittest
 from volume import cube_volume
 
 class TestVolume(unittest.TestCase):
-    def test_correct(self):
+    def test_float(self):
         self.assertEqual(cube_volume(1,2,1.5), 3.0)
+
     def test_negative(self):
         self.assertNotEqual(cube_volume(-1,2,3), -6)
 
+    def test_type(self):
+        self.assertRaises(TypeError, cube_volume, 'A', '2b', 3)
+    
 if __name__ == '__main__':
     unittest.main(verbosity=2)
