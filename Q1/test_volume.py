6f5234b26b320980eb179b31cda4e41eea244688
Garrett Scott <Garret-t@users.noreply.github.com>
2021-04-27 08:38:28 -0700

    Added test for negative values

--- Q1/test_volume.py
+++ Q1/test_volume.py
@@ -4,7 +4,8 @@ from volume import cube_volume
 class TestVolume(unittest.TestCase):
     def test_correct(self):
         self.assertEqual(cube_volume(1,2,1.5), 3.0)
-
+    def test_negative(self):
+        self.assertNotEqual(cube_volume(-1,2,3), -6)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
