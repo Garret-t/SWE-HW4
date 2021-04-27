88030191a816802e3f2da0b35f2891b44089aaf0
Garrett Scott <Garret-t@users.noreply.github.com>
2021-04-27 12:37:03 -0700

    Added 2 more tests for average

--- Q2/test_average.py
+++ Q2/test_average.py
@@ -4,6 +4,12 @@ from average import avg
 class TestAverage(unittest.TestCase):
     def test_empty(self):
         self.assertRaises(ZeroDivisionError, avg, [])
-    
+
+    def test_float_neg(self):
+        self.assertEqual(avg([.5,-.5, 3]), 1.0)
+
+    def test_args(self):
+        self.assertRaises(TypeError, avg, 1, 2, 3)
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
