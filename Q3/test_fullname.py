76a3575475666b18fb955e3e433f7e1c6920c1b2
Garrett Scott <Garret-t@users.noreply.github.com>
2021-04-27 17:10:34 -0700

    Added tests for letters and symbols in fullname

--- Q3/test_fullname.py
+++ Q3/test_fullname.py
@@ -5,5 +5,15 @@ class TestFullName(unittest.TestCase):
     def test_correct(self):
         self.assertEqual(gen_fullname("John", "Doe"), "John Doe")
 
+    def test_empty(self):
+        self.assertNotEqual(gen_fullname("", ""), " ")
+
+    def test_letters(self):
+        self.assertRegex(gen_fullname("John", "Doe"), "[a-zA-Z][a-zA-Z ]+[a-zA-Z]$")
+    
+    def test_symbols(self):
+        self.assertRegex(gen_fullname("J0hn", "Do3"), "[a-zA-Z][a-zA-Z ]+[a-zA-Z]$")
+    
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
